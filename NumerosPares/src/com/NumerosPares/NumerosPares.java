53e7c15cf8acb166a70928dca34aa717aad681ad
Johann Quezada <quezadajohann0x0x@gmail.com>
2025-09-27 14:28:45 -0400

    Ejercicio 4

---
+++ NumerosPares/src/com/NumerosPares/NumerosPares.java
@@ -0,0 +1,23 @@
+
+package com.NumerosPares;
+
+/**
+ *
+ * @author queza
+ */
+public class NumerosPares {
+    
+    public static void main(String[] args) {
+        
+        //4. Realizar un programa que imprima los números pares del 2 al 100 utilizando ciclos "while".
+        
+        System.out.println("Numeros pares del 2 al 100:");
+
+        int i = 2; 
+        while (i <= 100) {
+            System.out.println(i);
+            i += 2; 
+        }
+    }
+    
+}
