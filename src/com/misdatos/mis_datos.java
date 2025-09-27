81d01a245599e82828da36f9ad4af47543bfebe6
JohannQuezada <quezadajohann0x0x@gmail.com>
2025-09-27 13:29:20 -0400

    Ejercicio 1 - Datos Personales

---
+++ src/com/misdatos/mis_datos.java
@@ -0,0 +1,16 @@
+package com.misdatos;
+
+/**
+ *
+ * @author queza
+ */
+public class mis_datos {
+  
+//1. Realizar un programa que imprima por pantalla su nombre y correo electrónico.  
+    
+    public static void main(String[] args) {
+       
+        System.out.println("Johann Daniel Quezada Collado");
+        System.out.println("quezadajohann0x0x@gmail.com");
+    }
+}
