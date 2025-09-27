7e71668792f65571e5ad88dc27f0ebdd6ce73385
Johann Quezada <quezadajohann0x0x@gmail.com>
2025-09-27 14:24:33 -0400

    Ejercicio 2

---
+++ Operaciones basicas/src/com/operaciones/operaciones.java
@@ -0,0 +1,22 @@
+
+package com.operaciones;
+
+/**
+ *
+ * @author queza
+ */
+public class operaciones {
+    
+    //2. Realizar un programa que incluya las 4 operaciones matemáticas básicas (Suma, Resta, Multiplicación y División).
+    
+    public static void main(String[] args) {
+        int valor1= 18;
+        int valor2= 3;
+        
+        System.out.println(valor1+"+"+valor2+"="+(valor1+valor2));
+        System.out.println(valor1+"-"+valor2+"="+(valor1-valor2));
+        System.out.println(valor1+"*"+valor2+"="+(valor1*valor2));
+        System.out.println(valor1+"/"+valor2+"="+(valor1/valor2));
+    }
+ 
+}
